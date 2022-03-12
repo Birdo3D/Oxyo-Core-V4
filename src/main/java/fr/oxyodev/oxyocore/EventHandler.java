32831d93e3c9a58f1db282c6494d8cb943c1531f
Birdo3D <52548243+Birdo3D@users.noreply.github.com>
2022-03-12 20:27:31 +0100

    Ore Generator

--- src/main/java/fr/oxyodev/oxyocore/EventHandler.java
+++ src/main/java/fr/oxyodev/oxyocore/EventHandler.java
@@ -1,16 +1,21 @@
 package fr.oxyodev.oxyocore;
 
 import fr.birdo.easycraftapi.entity.PlayerHelper;
+import fr.birdo.easycraftapi.util.Random;
 import fr.oxyodev.oxyocore.commands.CommandPvP;
 import fr.oxyodev.oxyocore.guis.GuiGenerator;
 import fr.oxyodev.oxyocore.utils.GeneratorData;
 import fr.oxyodev.oxyocore.utils.GeneratorTier;
 import fr.oxyodev.oxyocore.utils.Utils;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockFormEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -18,7 +23,10 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 public class EventHandler implements Listener {
 
-    public EventHandler(OxyoCore oxyoCore) {
+    private static OxyoCore instance;
+
+    public EventHandler(OxyoCore pluginInstance) {
+        instance = pluginInstance;
     }
 
     @org.bukkit.event.EventHandler
@@ -63,5 +71,51 @@ public class EventHandler implements Listener {
                         }
                         event.setCancelled(true);
                     }
+    }
+
+    @org.bukkit.event.EventHandler
+    public void onBlockFormEvent(BlockFormEvent e) {
+        Block block = e.getBlock();
+        if (block.getType() == Material.LAVA && findNearbyLava(block)) {
+            int max = GeneratorData.getGeneratorTier() * 100;
+            if (GeneratorData.getGeneratorTier() == 7 && GeneratorData.getGeneratorAdvancement() == GeneratorTier.EMERALD.getCount())
+                max = max + 100;
+            int rand = Random.roll(1, max);
+            if (rand <= GeneratorTier.COAL.getSpawnChance() && GeneratorData.getGeneratorTier() >= 1) {
+                block.setType(Material.COAL_ORE);
+                e.setCancelled(true);
+            } else if (rand > 100 && rand <= GeneratorTier.COPPER.getSpawnChance() + 100 && GeneratorData.getGeneratorTier() >= 2) {
+                block.setType(Material.COPPER_ORE);
+                e.setCancelled(true);
+            } else if (rand > 200 && rand <= GeneratorTier.LAPIS.getSpawnChance() + 200 && GeneratorData.getGeneratorTier() >= 3) {
+                block.setType(Material.LAPIS_ORE);
+                e.setCancelled(true);
+            } else if (rand > 300 && rand <= GeneratorTier.IRON.getSpawnChance() + 300 && GeneratorData.getGeneratorTier() >= 4) {
+                block.setType(Material.IRON_ORE);
+                e.setCancelled(true);
+            } else if (rand > 400 && rand <= GeneratorTier.GOLD.getSpawnChance() + 400 && GeneratorData.getGeneratorTier() >= 5) {
+                block.setType(Material.GOLD_ORE);
+                e.setCancelled(true);
+            } else if (rand > 500 && rand <= GeneratorTier.REDSTONE.getSpawnChance() + 500 && GeneratorData.getGeneratorTier() >= 6) {
+                block.setType(Material.REDSTONE_ORE);
+                e.setCancelled(true);
+            } else if (rand > 600 && rand <= GeneratorTier.DIAMOND.getSpawnChance() + 600 && GeneratorData.getGeneratorTier() == 7) {
+                block.setType(Material.DIAMOND_ORE);
+                e.setCancelled(true);
+            } else if (rand > 700 && rand <= GeneratorTier.EMERALD.getSpawnChance() + 700 && GeneratorData.getGeneratorTier() == 7 && GeneratorData.getGeneratorAdvancement() == GeneratorTier.EMERALD.getCount()) {
+                block.setType(Material.EMERALD_ORE);
+                e.setCancelled(true);
+            }
+        }
+    }
+
+    public static boolean findNearbyLava(Block block) {
+        int[][] nearby = new int[][]{{-1, 0, 0}, {0, -1, 0}, {0, 0, -1}, {1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+        int x = block.getX(), y = block.getY(), z = block.getZ();
+        World world = block.getWorld();
+        for (int[] i : nearby)
+            if (world.getBlockAt(x + i[0], y + i[1], z + i[2]).getType() == Material.LAVA)
+                return true;
+        return false;
     }
 }
